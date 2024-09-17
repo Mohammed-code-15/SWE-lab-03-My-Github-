2fbbf3a2366f80a8e3378245bbc2786d7f48dc0d
John Doe <johndoe@email.com>
2024-09-17 15:23:29 +0300

    adding display method

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         display("Hello");
+        
     }
 
     public static void display(String mas) {
         System.out.println(mas);
     }
+
+    
 
 }
