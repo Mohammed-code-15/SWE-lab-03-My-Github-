4f60061e4862e412108bbf65a1f0e5f20f368042
John Doe <johndoe@email.com>
2024-09-17 15:22:05 +0300

    adding readList method77

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        display("Hello");
+    }
+
+    public static void display(String mas) {
+        System.out.println(mas);
+    }
+
+}
