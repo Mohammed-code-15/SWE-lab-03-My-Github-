2c97f2fd8ab3b3a13d28b7050aab517925b73b62
John Doe <johndoe@email.com>
2024-09-17 15:24:36 +0300

    adding readList method 2

--- Main.java
+++ Main.java
@@ -3,13 +3,22 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         display("Hello");
-        
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Mohammed");
+        list.add("Ali");
+        readList(list);
     }
 
     public static void display(String mas) {
         System.out.println(mas);
     }
 
-    
+    public static void readList(ArrayList<String> list) {
+        int i = 1;
+        for(String el : list){
+            System.out.println(i + " " + el);
+            i ++;
+        }
+    }
 
 }
